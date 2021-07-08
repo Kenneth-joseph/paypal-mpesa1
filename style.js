be1a6f30145287a4d7ced40a0d89e3eca978c27c
Kenneth-joseph <josephochieng812@gmail.com>
2021-07-08 23:58:07 +0300

    added table to hold the account values

--- style.js
+++ style.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('button#btn').click(function(event){
         event.preventDefault();
     let amount=document.getElementById('amount').value
-    document.write(amount);
+    // document.write(amount);  
+    $('.table table').show();
     })
  });
