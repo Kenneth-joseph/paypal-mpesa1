c76db1a3bf43fecbef9f9463cd4dd0a5ae555aa2
Kenneth-joseph <josephochieng812@gmail.com>
2021-07-09 18:13:45 +0300

    completed the project essentials

--- style.js
+++ style.js
@@ -1,3 +1,4 @@
+// declaring the global neccessary variable 
 let paybill_charge=22;
 let usd= 100;
 localb= 40;
@@ -8,33 +9,53 @@ international_comission= 1.5;
 $(document).ready(function(){
     $('button#btn').click(function(event){
         event.preventDefault();
-    let amount=parseInt( document.getElementById('amount').value)    
+    let amount=parseInt( document.getElementById('amount').value)
+    // document.getElementById("pay_k").innerHTML = paypill;
+
+
      //mpesa
     let mpesa = amount
     let u_mpesa = amount/usd;
-    
+    $("#mpesa_k").html(mpesa);
+    $("#mpesa_u").html(u_mpesa);
+
     //paybill
     let paybill= mpesa + paybill_charge
     let paybill_usd= paybill/usd
-    
+    $("#pay_k").html(paybill);
+    $("#pay_u").html(paybill_usd);
 
     // local bank
     let localbank= paybill + localb;
     let localbank_usd = localbank/usd;
+    $("#local_k").html(localbank);
+    $("#local_u").html(localbank_usd);
     
 
     // international bank
     let commission = (international_comission * mpesa)/100;
     let ibank= localbank + commission;
     let ibank_usd= ibank/usd;
-    
+    $("#inter_k").html(ibank);
+    $("#inter_u").html(ibank_usd);
 
     // paypal charges
 
     let pcommission = (paypal_commission*ibank)/100;
     paypal = pcommission+ibank; 
     paypal_usd =paypal/usd;
-    alert(paypal_usd);
+    $("#fin_k").html(paypal);
+    $("#fin_u").html(paypal_usd);
+
+    // client paypal
+    // let pcommission = (paypal_commission*ibank)/100;
+    // paypal = pcommission+ibank; 
+    // paypal_usd =paypal/usd;
+    // $("#client_k").html(paypal);
+    // $("#client_u").html(paypal_usd);
+    // document.write(paypal_usd);
+
+    
 
 
 
