6ae24825df43a73db8b06ef646605070aa86567d
Kenneth-joseph <josephochieng812@gmail.com>
2021-07-09 16:12:01 +0300

    added the table

--- style.js
+++ style.js
@@ -2,7 +2,12 @@ $(document).ready(function(){
     $('button#btn').click(function(event){
         event.preventDefault();
     let amount=document.getElementById('amount').value
+    //mpesa
+    let mpesa = amount
+    let u_mpesa = amount* 100;
+
+    
     // document.write(amount);  
-    $('.table table').show();
+    // $('.table table').show();
     })
  });
